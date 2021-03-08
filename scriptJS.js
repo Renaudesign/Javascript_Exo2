196fb79d534b31b54874b8b80d25d6b8b64c2ba3
Renaud Sourmail <79084331+Renaudesign@users.noreply.github.com>
2021-03-08 13:48:04 +0100

    Update scriptJS.js

--- scriptJS.js
+++ scriptJS.js
@@ -1,6 +1,5 @@
 // @ts-check
 let fnameValue = prompt("Please enter your name", "Bob Ross")
-
 if (fnameValue != null) {
     alert("Bonjour " + fnameValue + "! Comment allez-vous par ce matin radieux?")
   }
